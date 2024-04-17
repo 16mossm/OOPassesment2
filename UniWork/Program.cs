8bc3ef13dc85681340a7c47d336faca224af3781
16mossm <50373533+16mossm@users.noreply.github.com>
2024-04-17 18:44:45 +0100

    added statistics

--- UniWork/Program.cs
+++ UniWork/Program.cs
@@ -10,11 +10,9 @@ namespace UniWork
     {
         static void Main(string[] args)
         {
+            Statistics stats = new Statistics();
 
-            int players = 0;
-            int bots = 0;
-
-            Player[] Players = new Player[2];
+            Player[] players = new Player[2];
 
             Console.WriteLine("how many players? {1,2}");
             string Userinput = Console.ReadLine();
@@ -26,12 +24,12 @@ namespace UniWork
             {
                 Console.WriteLine("enter player 1's name:");
                 string p1 = Console.ReadLine();
-                Players = new Player[]
+                players = new Player[]
                 {
                     new Player(p1),
                     new Player("bot")
                 };
-                Players[1].bot = true;
+                players[1].bot = true;
             }
             if (Userinput == "2")
             {
@@ -39,7 +37,7 @@ namespace UniWork
                 string p1 = Console.ReadLine();
                 Console.WriteLine("enter player 2's name:");
                 string p2 = Console.ReadLine();
-                Players = new Player[]
+                players = new Player[]
                 {
                     new Player(p1),
                     new Player(p2)
@@ -49,7 +47,11 @@ namespace UniWork
             bool quit = false;
             while (!quit)
             {
-                Console.WriteLine("what game would you like to play {1 = SevensOut, 2 = ThreeOrMore, quit = quit}");
+                Console.WriteLine("- - - - - - -");
+                
+                
+
+                Console.WriteLine("what game would you like to play {1 = SevensOut, 2 = ThreeOrMore, quit = quit , stats = show stats}");
                 string GameChoice = Console.ReadLine();
                 while (GameChoice != "1" && GameChoice != "2")
                 {
@@ -58,10 +60,21 @@ namespace UniWork
                         break;
                             }
 
-                    Console.WriteLine("how many players? {1,2}");
-                    Userinput = Console.ReadLine();
+                    if (GameChoice == "stats")
+                    {
+                        Console.WriteLine("---stats---");
+                        Console.WriteLine("SevensOut:");
+                        Console.WriteLine("highscore: "+stats.getScore(new SevensOut()).ToString());
+                        Console.WriteLine("plays: " + stats.getPlays(new SevensOut()).ToString());
+                        Console.WriteLine("ThreeOrMore:");
+                        Console.WriteLine("highscore: " + stats.getScore(new ThreeOrMore()).ToString());
+                        Console.WriteLine("plays: " + stats.getPlays(new ThreeOrMore()).ToString());
+                    }
+
+                    Console.WriteLine("what game would you like to play {1 = SevensOut, 2 = ThreeOrMore, quit = quit}");
+                    GameChoice = Console.ReadLine();
                 }
-                Game gameplaying;
+                Game gameplaying = null;
                 if(GameChoice == "1")
                 {
                     gameplaying = new SevensOut();
@@ -74,8 +87,10 @@ namespace UniWork
 
                 ////////polymorphism
                 // have the statistics class play the game and save the score because thats the funny way to do this
-
-
+                if (!quit)
+                {
+                    stats.playGame(gameplaying, players);
+                }
 
             }
 
--- UniWork/Statistics.cs
+++ UniWork/Statistics.cs
@@ -13,5 +13,46 @@ namespace UniWork
         public int[] highscores;
         public int[] ammountOfPlays;
 
+        public Statistics(int ammountOfGames = 2)
+        {
+            highscores = new int[2] { 0,0};
+            ammountOfPlays = new int[2] { 0, 0 };
+        }
+
+        public void playGame(Game game, Player[] players)
+        {
+            //sevensout = 0
+            //threeormore = 1
+            int score = game.playGame(players);
+            int gameID = getGameID(game);
+
+            if (highscores[gameID] < score)
+            {
+                highscores[gameID] = score;
+            }
+            ammountOfPlays[gameID]++;
+        }
+
+        int getGameID(Game game) {
+            if (game is SevensOut)
+            {
+                return 0;
+            }
+
+            if (game is ThreeOrMore)
+            {
+                return 1;
+            }
+            return -1;
+        }
+        public int getScore(Game game)
+        {
+            return highscores[getGameID(game)];
+        }
+        public int getPlays(Game game)
+        {
+            return ammountOfPlays[getGameID(game)];
+        }
+
     }
 }
